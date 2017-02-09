a453b8f4b4f5416099342207eac891ef2c9e5dc8
rikumax25 <jorgev259@outlook.es>
2017-02-08 21:35:27 -0600

    For you, JK

--- app.js
+++ app.js
@@ -62,9 +62,8 @@ function serializeForm() {
 }
 
 function cleanup(stamp) {
-    $.ajax({
-        url: "https://sdhelper.azurewebsites.net/api/cleaner/", // be consistent and case the route the same as the ApiController
-        type: "POST",
+    $.post({
+        url: "https://sdhelper.azurewebsites.net/api/cleaner/",
         dataType: 'json',
         data: "=" + stamp, // add an = sign
         success: function (result) {
